b2381b98c0e57226e188491634fc7c3f6173e8b3
Ties Bolding <tiesnld+git@gmail.com>
2023-12-05 16:19:23 +0100

    Improve day 03 performance

--- README.md
+++ README.md
@@ -5,15 +5,17 @@
 Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.rust-lang.org/).
 
 <!--- advent_readme_stars table --->
+
 ## 2023 Results
 
-| Day | Part 1 | Part 2 |
-| :---: | :---: | :---: |
-| [Day 1](https://adventofcode.com/2023/day/1) | ⭐ | ⭐ |
-| [Day 2](https://adventofcode.com/2023/day/2) | ⭐ | ⭐ |
-| [Day 3](https://adventofcode.com/2023/day/3) | ⭐ | ⭐ |
-| [Day 4](https://adventofcode.com/2023/day/4) | ⭐ | ⭐ |
-| [Day 5](https://adventofcode.com/2023/day/5) | ⭐ | ⭐ |
+|                     Day                      | Part 1 | Part 2 |
+| :------------------------------------------: | :----: | :----: |
+| [Day 1](https://adventofcode.com/2023/day/1) |   ⭐   |   ⭐   |
+| [Day 2](https://adventofcode.com/2023/day/2) |   ⭐   |   ⭐   |
+| [Day 3](https://adventofcode.com/2023/day/3) |   ⭐   |   ⭐   |
+| [Day 4](https://adventofcode.com/2023/day/4) |   ⭐   |   ⭐   |
+| [Day 5](https://adventofcode.com/2023/day/5) |   ⭐   |   ⭐   |
+
 <!--- advent_readme_stars table --->
 
 <!--- benchmarking table --->
@@ -21,11 +23,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `161.0µs` | `841.4µs` |
-| [Day 2](./src/bin/02.rs) | `281.0ns` | `22.0ns` |
-| [Day 3](./src/bin/03.rs) | `246.7µs` | `7.6ms` |
-| [Day 4](./src/bin/04.rs) | `30.9µs` | `50.1µs` |
-| [Day 5](./src/bin/05.rs) | `1.9µs` | `30.8µs` |
+| [Day 1](./src/bin/01.rs) | `141.7µs` | `795.1µs` |
+| [Day 2](./src/bin/02.rs) | `277.0ns` | `22.0ns` |
+| [Day 3](./src/bin/03.rs) | `414.7µs` | `985.2µs` |
+| [Day 4](./src/bin/04.rs) | `30.6µs` | `49.3µs` |
+| [Day 5](./src/bin/05.rs) | `2.0µs` | `32.1µs` |
 
-**Total: 8.96ms**
+**Total: 2.45ms**
 <!--- benchmarking table --->
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,18 +1,25 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(3);
 
-type Position = (i32, i32);
+type Position = (usize, usize);
+type OldPosition = (i32, i32);
 
-type Input = (HashMap<Position, String>, HashMap<Position, char>);
+type Input = Vec<Vec<char>>;
 
 pub fn parse_input(input: String) -> Input {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+pub fn old_parse_input(
+    input: &Input,
+) -> (HashMap<OldPosition, String>, HashMap<OldPosition, char>) {
     let mut res = (HashMap::new(), HashMap::new());
-    for (y, line) in input.lines().enumerate() {
+    for (y, line) in input.iter().enumerate() {
         let mut t = String::new();
-        for (x, c) in line.char_indices() {
+        for (x, c) in line.iter().enumerate() {
             if c.is_ascii_digit() {
-                t.push(c);
+                t.push(*c);
             } else {
                 if !t.is_empty() {
                     res.0.insert(
@@ -24,9 +31,9 @@ pub fn parse_input(input: String) -> Input {
                     );
                     t.clear();
                 }
-                if c != '.' {
+                if *c != '.' {
                     res.1
-                        .insert((i32::try_from(x).unwrap(), i32::try_from(y).unwrap()), c);
+                        .insert((i32::try_from(x).unwrap(), i32::try_from(y).unwrap()), *c);
                 }
             }
         }
@@ -44,7 +51,24 @@ pub fn parse_input(input: String) -> Input {
     res
 }
 
+fn neighbors((x, y): &Position) -> HashSet<Position> {
+    [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1),
+    ]
+    .iter()
+    .map(|&(dx, dy)| (x.saturating_add_signed(dx), y.saturating_add_signed(dy)))
+    .collect()
+}
+
 pub fn part_one(input: &Input) -> Option<u32> {
+    let parsed_input = old_parse_input(input);
     let mut res = 0;
     let ds: Vec<(i32, i32)> = vec![
         (-1, 0),
@@ -56,12 +80,12 @@ pub fn part_one(input: &Input) -> Option<u32> {
         (1, -1),
         (1, 1),
     ];
-    'outer: for (pos, part) in &input.0 {
+    'outer: for (pos, part) in &parsed_input.0 {
         for i in 0..part.len() {
             let x = pos.0 + i32::try_from(i).unwrap();
             let y = pos.1;
             for d in &ds {
-                if input.1.contains_key(&(x + d.0, y + d.1)) {
+                if parsed_input.1.contains_key(&(x + d.0, y + d.1)) {
                     res += part.parse::<u32>().unwrap();
                     continue 'outer;
                 }
@@ -73,39 +97,55 @@ pub fn part_one(input: &Input) -> Option<u32> {
 
 // I'll need to optimize this one at some point
 pub fn part_two(input: &Input) -> Option<u32> {
-    let mut res = 0;
-    let ds: Vec<(i32, i32)> = vec![
-        (-1, 0),
-        (1, 0),
-        (0, -1),
-        (0, 1),
-        (-1, -1),
-        (-1, 1),
-        (1, -1),
-        (1, 1),
-    ];
-    for (cpos, c) in &input.1 {
-        if c.ne(&'*') {
-            continue;
-        }
+    let width = input[0].len();
+    let height = input.len();
 
-        let mut parts = vec![];
-        'parts: for (ppos, part) in &input.0 {
-            for d in &ds {
-                for i in 0..part.len() {
-                    if ppos.0 + i32::try_from(i).unwrap() == cpos.0 + d.0 && ppos.1 == cpos.1 + d.1
-                    {
-                        parts.push(part.clone());
-                        continue 'parts;
+    let mut res = 0;
+
+    let mut symbols: HashMap<Position, (char, Vec<u32>)> = HashMap::new();
+
+    let mut value = 0;
+    let mut nearby: HashSet<Position> = HashSet::new();
+
+    for (y, line) in input.iter().enumerate() {
+        let mut x = 0;
+        while x < width {
+            while x < width && line[x].is_ascii_digit() {
+                value = value * 10 + line[x].to_digit(10).unwrap();
+                for (xx, yy) in neighbors(&(x, y)) {
+                    if xx < width && yy < height {
+                        let c = input[yy][xx];
+                        if c != '.' && !c.is_ascii_digit() {
+                            nearby.insert((xx, yy));
+                        }
                     }
                 }
+
+                x += 1;
             }
-        }
-        if parts.len() == 2 {
-            res += parts.first().unwrap().parse::<u32>().unwrap()
-                * parts.get(1).unwrap().parse::<u32>().unwrap();
+
+            if !nearby.is_empty() {
+                for position in &nearby {
+                    symbols
+                        .entry(*position)
+                        .or_insert((input[position.1][position.0], vec![]))
+                        .1
+                        .push(value);
+                }
+            }
+
+            x += 1;
+            value = 0;
+            nearby.clear();
         }
     }
+
+    for (_position, (c, values)) in symbols {
+        if c == '*' && values.len() == 2 {
+            res += values[0] * values[1];
+        }
+    }
+
     Some(res)
 }
 
