c4a317b9a755d668a7d5c867286e267b7c3b0484
Ties Bolding <tiesnld+git@gmail.com>
2023-12-14 12:02:06 +0100

    Day 14

--- README.md
+++ README.md
@@ -29,19 +29,20 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `31.4µs` | `181.6µs` |
-| [Day 2](./src/bin/02.rs) | `33.4µs` | `34.1µs` |
-| [Day 3](./src/bin/03.rs) | `486.4µs` | `877.3µs` |
-| [Day 4](./src/bin/04.rs) | `118.2µs` | `329.9µs` |
-| [Day 5](./src/bin/05.rs) | `20.7µs` | `28.3µs` |
-| [Day 6](./src/bin/06.rs) | `237.0ns` | `528.0ns` |
-| [Day 7](./src/bin/07.rs) | `375.7µs` | `379.2µs` |
-| [Day 8](./src/bin/08.rs) | `305.6µs` | `805.3µs` |
-| [Day 9](./src/bin/09.rs) | `128.5µs` | `134.3µs` |
-| [Day 10](./src/bin/10.rs) | `838.1µs` | `1.3ms` |
-| [Day 11](./src/bin/11.rs) | `132.7µs` | `1.6ms` |
-| [Day 12](./src/bin/12.rs) | `1.2ms` | `22.3ms` |
-| [Day 13](./src/bin/13.rs) | `151.6µs` | `159.6µs` |
+| [Day 1](./src/bin/01.rs) | `31.1µs` | `181.2µs` |
+| [Day 2](./src/bin/02.rs) | `34.0µs` | `34.5µs` |
+| [Day 3](./src/bin/03.rs) | `489.2µs` | `892.9µs` |
+| [Day 4](./src/bin/04.rs) | `234.7µs` | `332.9µs` |
+| [Day 5](./src/bin/05.rs) | `21.2µs` | `29.2µs` |
+| [Day 6](./src/bin/06.rs) | `206.0ns` | `503.0ns` |
+| [Day 7](./src/bin/07.rs) | `397.8µs` | `383.9µs` |
+| [Day 8](./src/bin/08.rs) | `313.6µs` | `796.9µs` |
+| [Day 9](./src/bin/09.rs) | `132.9µs` | `136.7µs` |
+| [Day 10](./src/bin/10.rs) | `841.8µs` | `1.3ms` |
+| [Day 11](./src/bin/11.rs) | `133.2µs` | `1.6ms` |
+| [Day 12](./src/bin/12.rs) | `1.2ms` | `23.0ms` |
+| [Day 13](./src/bin/13.rs) | `154.3µs` | `160.8µs` |
+| [Day 14](./src/bin/14.rs) | `26.5µs` | `47.9ms` |
 
-**Total: 31.95ms**
+**Total: 80.76ms**
 <!--- benchmarking table --->
---
+++ src/bin/14.rs
@@ -0,0 +1,157 @@
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    hash::{DefaultHasher, Hash, Hasher},
+};
+
+advent_of_code::solution!(14);
+
+type GridT = Vec<Vec<char>>;
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+struct Grid {
+    grid: GridT,
+}
+
+impl Grid {
+    pub fn rotated_cw(&self) -> Grid {
+        let mut rotated: GridT = (0..self.grid[0].len()).map(|_| vec![]).collect();
+
+        self.grid.iter().rev().for_each(|original_row| {
+            for (item, transposed_row) in original_row.iter().zip(&mut rotated) {
+                transposed_row.push(*item);
+            }
+        });
+
+        Grid { grid: rotated }
+    }
+
+    pub fn tilted(&self) -> Grid {
+        let width = self.grid[0].len();
+        let mut rocks: Vec<usize> = vec![0; width];
+        let mut new_grid = self.clone();
+
+        for (y, row) in self.grid.iter().enumerate() {
+            for (x, c) in row.iter().enumerate() {
+                match *c {
+                    'O' => {
+                        new_grid.grid[y][x] = '.';
+                        new_grid.grid[rocks[x]][x] = 'O';
+                        rocks[x] += 1;
+                    }
+                    '#' => {
+                        rocks[x] = y + 1;
+                    }
+                    _ => (),
+                }
+            }
+        }
+
+        new_grid
+    }
+
+    pub fn load(&self) -> usize {
+        let height = self.grid.len();
+
+        let mut res = 0;
+        for (y, row) in self.grid.iter().enumerate() {
+            for c in row {
+                if *c == 'O' {
+                    res += height - y;
+                }
+            }
+        }
+
+        res
+    }
+}
+
+impl From<&str> for Grid {
+    fn from(s: &str) -> Grid {
+        Grid {
+            grid: s.lines().map(|line| line.chars().collect()).collect(),
+        }
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let strings: Vec<String> = self.grid.iter().map(|line| line.iter().collect()).collect();
+        write!(f, "{}", strings.join("\n"))
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let grid: Grid = Grid::from(input);
+    let height = grid.grid.len();
+    let width = grid.grid[0].len();
+
+    let mut rocks: Vec<usize> = vec![0; width];
+
+    let mut res = 0;
+    for (y, row) in grid.grid.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            match *c {
+                'O' => {
+                    res += height - rocks[x];
+                    rocks[x] += 1;
+                }
+                '#' => {
+                    rocks[x] = y + 1;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    Some(res)
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let n = 1000000000;
+    let mut grid: Grid = Grid::from(input);
+
+    let mut m: HashMap<u64, usize> = HashMap::new();
+
+    for cycle in 1..=n {
+        for _ in 0..4 {
+            grid = grid.tilted().rotated_cw()
+        }
+
+        let hash = calculate_hash(&grid);
+        if let Some(prev) = m.get(&hash) {
+            let cycle_length = cycle - prev;
+
+            if (n - prev) % cycle_length == 0 {
+                return Some(grid.load());
+            }
+        } else {
+            m.insert(hash, cycle);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(136));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(64));
+    }
+}
