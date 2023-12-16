5950593ffd99e2a81b60e4f23ff24ece38779963
Ties Bolding <tiesnld+git@gmail.com>
2023-12-16 10:28:54 +0100

    Day 16

--- README.md
+++ README.md
@@ -31,21 +31,22 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `35.2µs` | `200.3µs` |
-| [Day 2](./src/bin/02.rs) | `37.0µs` | `38.2µs` |
-| [Day 3](./src/bin/03.rs) | `543.8µs` | `979.6µs` |
-| [Day 4](./src/bin/04.rs) | `68.2µs` | `367.9µs` |
-| [Day 5](./src/bin/05.rs) | `23.0µs` | `31.5µs` |
-| [Day 6](./src/bin/06.rs) | `241.0ns` | `550.0ns` |
-| [Day 7](./src/bin/07.rs) | `414.5µs` | `415.9µs` |
-| [Day 8](./src/bin/08.rs) | `339.5µs` | `908.7µs` |
-| [Day 9](./src/bin/09.rs) | `146.5µs` | `150.3µs` |
-| [Day 10](./src/bin/10.rs) | `946.3µs` | `1.5ms` |
-| [Day 11](./src/bin/11.rs) | `145.9µs` | `1.8ms` |
-| [Day 12](./src/bin/12.rs) | `1.5ms` | `27.2ms` |
-| [Day 13](./src/bin/13.rs) | `165.4µs` | `175.3µs` |
-| [Day 14](./src/bin/14.rs) | `27.5µs` | `53.2ms` |
-| [Day 15](./src/bin/15.rs) | `61.0µs` | `247.1µs` |
+| [Day 1](./src/bin/01.rs) | `30.8µs` | `183.3µs` |
+| [Day 2](./src/bin/02.rs) | `34.0µs` | `33.7µs` |
+| [Day 3](./src/bin/03.rs) | `491.9µs` | `896.6µs` |
+| [Day 4](./src/bin/04.rs) | `78.0µs` | `332.5µs` |
+| [Day 5](./src/bin/05.rs) | `21.0µs` | `29.2µs` |
+| [Day 6](./src/bin/06.rs) | `217.0ns` | `548.0ns` |
+| [Day 7](./src/bin/07.rs) | `374.5µs` | `420.1µs` |
+| [Day 8](./src/bin/08.rs) | `320.4µs` | `1.0ms` |
+| [Day 9](./src/bin/09.rs) | `132.2µs` | `136.2µs` |
+| [Day 10](./src/bin/10.rs) | `886.0µs` | `1.4ms` |
+| [Day 11](./src/bin/11.rs) | `147.0µs` | `1.8ms` |
+| [Day 12](./src/bin/12.rs) | `1.2ms` | `24.7ms` |
+| [Day 13](./src/bin/13.rs) | `155.0µs` | `165.0µs` |
+| [Day 14](./src/bin/14.rs) | `25.9µs` | `47.9ms` |
+| [Day 15](./src/bin/15.rs) | `55.0µs` | `229.2µs` |
+| [Day 16](./src/bin/16.rs) | `192.5µs` | `7.8ms` |
 
-**Total: 91.67ms**
+**Total: 91.17ms**
 <!--- benchmarking table --->
---
+++ src/bin/16.rs
@@ -0,0 +1,172 @@
+use rayon::prelude::*;
+
+advent_of_code::solution!(16);
+
+#[derive(Clone, Copy)]
+enum Direction {
+    Horizontal(isize),
+    Vertical(isize),
+}
+
+impl Direction {
+    pub fn get_bd(&self) -> u8 {
+        match self {
+            Direction::Horizontal(1) => 0b1000,
+            Direction::Vertical(1) => 0b0010,
+            Direction::Horizontal(-1) => 0b0100,
+            Direction::Vertical(-1) => 0b0001,
+            _ => panic!(),
+        }
+    }
+}
+
+type Position = (isize, isize);
+
+type Beam = (Position, Direction);
+
+type Map = Vec<Vec<(char, u8)>>;
+
+fn run(i_map: &Map, starting_beam: Beam) -> usize {
+    let mut map = i_map.clone();
+    let mut beams: Vec<Beam> = vec![starting_beam];
+    while !beams.is_empty() {
+        let mut new_beams: Vec<Beam> = vec![];
+        for (pos, dir) in &mut beams {
+            match dir {
+                Direction::Horizontal(d) => {
+                    pos.0 += *d;
+                }
+                Direction::Vertical(d) => {
+                    pos.1 += *d;
+                }
+            }
+
+            if pos.0 < 0
+                || pos.0 >= map[0].len().try_into().unwrap()
+                || pos.1 < 0
+                || pos.1 >= map.len().try_into().unwrap()
+            {
+                continue;
+            }
+
+            let x = usize::try_from(pos.0).unwrap();
+            let y = usize::try_from(pos.1).unwrap();
+
+            let cur = map[y][x];
+
+            if cur.1 & dir.get_bd() > 0 {
+                continue;
+            }
+
+            map[y][x] = match cur {
+                ('\\', b) => {
+                    match dir {
+                        Direction::Horizontal(d) => new_beams.push((*pos, Direction::Vertical(*d))),
+                        Direction::Vertical(d) => new_beams.push((*pos, Direction::Horizontal(*d))),
+                    }
+
+                    ('\\', b | dir.get_bd())
+                }
+                ('/', b) => {
+                    match dir {
+                        Direction::Horizontal(d) => {
+                            new_beams.push((*pos, Direction::Vertical(-*d)))
+                        }
+                        Direction::Vertical(d) => {
+                            new_beams.push((*pos, Direction::Horizontal(-*d)))
+                        }
+                    }
+
+                    ('/', b | dir.get_bd())
+                }
+                ('|', b) => {
+                    if let Direction::Horizontal(_) = dir {
+                        new_beams.push((*pos, Direction::Vertical(-1)));
+                        new_beams.push((*pos, Direction::Vertical(1)));
+                    } else {
+                        new_beams.push((*pos, *dir));
+                    }
+
+                    ('|', b | dir.get_bd())
+                }
+                ('-', b) => {
+                    if let Direction::Vertical(_) = dir {
+                        new_beams.push((*pos, Direction::Horizontal(-1)));
+                        new_beams.push((*pos, Direction::Horizontal(1)));
+                    } else {
+                        new_beams.push((*pos, *dir));
+                    }
+
+                    ('-', b | dir.get_bd())
+                }
+                (c, b) => {
+                    new_beams.push((*pos, *dir));
+                    (c, b | dir.get_bd())
+                }
+            }
+        }
+        beams = new_beams;
+    }
+
+    map.iter()
+        .map(|row| row.iter().filter(|(_, energized)| *energized > 0).count())
+        .sum()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let map: Map = input
+        .lines()
+        .map(|line| line.chars().map(|c| (c, 0b0000)).collect())
+        .collect();
+
+    Some(run(&map, ((-1, 0), Direction::Horizontal(1))))
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let map: Map = input
+        .lines()
+        .map(|line| line.chars().map(|c| (c, 0b0000)).collect())
+        .collect();
+
+    let height = map.len();
+    let width = map[0].len();
+
+    let mut starting_beams: Vec<Beam> = vec![];
+
+    for y in 0..height {
+        starting_beams.push(((-1, y.try_into().unwrap()), Direction::Horizontal(1)));
+        starting_beams.push((
+            (width.try_into().unwrap(), y.try_into().unwrap()),
+            Direction::Horizontal(-1),
+        ));
+    }
+    for x in 0..width {
+        starting_beams.push(((x.try_into().unwrap(), -1), Direction::Vertical(1)));
+        starting_beams.push((
+            (x.try_into().unwrap(), height.try_into().unwrap()),
+            Direction::Vertical(-1),
+        ));
+    }
+
+    starting_beams
+        .par_iter()
+        .map(|starting_beam| run(&map, *starting_beam))
+        .max()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(46));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(51));
+    }
+}
