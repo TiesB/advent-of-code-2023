617d73f8ad91508c51782291d470b6229b6780e3
Ties Bolding <tiesnld+git@gmail.com>
2023-12-18 12:19:37 +0100

    Improve day 18 performance slightly

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,63 +1,31 @@
 advent_of_code::solution!(18);
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-enum Direction {
-    Horizontal(isize),
-    Vertical(isize),
-}
+const DIRS: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-impl Direction {
-    pub fn get_d(&self) -> (isize, isize) {
-        match self {
-            Direction::Horizontal(d) => (*d, 0),
-            Direction::Vertical(d) => (0, *d),
-        }
-    }
-}
-
-type Instruction = (Direction, isize);
-
-fn parse_line(line: &str, use_color: bool) -> Instruction {
-    let parts: Vec<&str> = line.split_whitespace().collect();
-
-    if !use_color {
-        let i = parts[0].chars().collect::<Vec<char>>()[0];
-        let n = parts[1].parse::<isize>().unwrap();
-        let dir = match i {
-            'U' => Direction::Vertical(-1),
-            'D' => Direction::Vertical(1),
-            'L' => Direction::Horizontal(-1),
-            'R' => Direction::Horizontal(1),
-            _ => panic!(),
-        };
-        (dir, n)
-    } else {
-        let cn = isize::from_str_radix(&parts[2][2..7], 16).unwrap();
-        let cd = match parts[2].chars().nth(7).unwrap().to_digit(16).unwrap() {
-            3 => Direction::Vertical(-1),
-            1 => Direction::Vertical(1),
-            2 => Direction::Horizontal(-1),
-            0 => Direction::Horizontal(1),
-            _ => panic!(),
-        };
-
-        (cd, cn)
-    }
-}
-
-fn parse_lines(input: &str, use_color: bool) -> Vec<Instruction> {
-    input
-        .lines()
-        .map(|line| parse_line(line, use_color))
-        .collect()
-}
-
-fn solve(instructions: &Vec<Instruction>) -> isize {
+fn solve(input: &str, use_color: bool) -> isize {
     let mut pos = (0, 0);
     let mut prev_pos = (0, 0);
     let mut area = 0;
-    for (dir, n) in instructions {
-        let d = dir.get_d();
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let (d, n) = if !use_color {
+            (
+                DIRS[match parts[0] {
+                    "R" => 0,
+                    "D" => 1,
+                    "L" => 2,
+                    "U" => 3,
+                    _ => panic!(),
+                }],
+                parts[1].parse::<isize>().unwrap(),
+            )
+        } else {
+            (
+                DIRS[usize::try_from(parts[2].chars().nth(7).unwrap().to_digit(16).unwrap())
+                    .unwrap()],
+                isize::from_str_radix(&parts[2][2..7], 16).unwrap(),
+            )
+        };
 
         pos.0 += d.0 * n;
         pos.1 += d.1 * n;
@@ -69,11 +37,11 @@ fn solve(instructions: &Vec<Instruction>) -> isize {
 }
 
 pub fn part_one(input: &str) -> Option<isize> {
-    Some(solve(&parse_lines(input, false)))
+    Some(solve(input, false))
 }
 
 pub fn part_two(input: &str) -> Option<isize> {
-    Some(solve(&parse_lines(input, true)))
+    Some(solve(input, true))
 }
 
 #[cfg(test)]
